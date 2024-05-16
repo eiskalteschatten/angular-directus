9d15754ebefe5dafb6ad95cf67b3c65352d68d1e
Alex Seifert <alex@alexseifert.com>
2024-05-16 11:52:10 +0200

    Re-add missing files

---
+++ nuxt/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (subTitle) => {
+    return subTitle
+      ? `${subTitle} - Directus Blog`
+      : 'Directus Blog';
+  },
+  meta: [
+    { name: 'description', content: 'My amazing site.' },
+  ],
+});
+</script>
+
+<template>
+  <PageHeader />
+  <NuxtPage />
+</template>
