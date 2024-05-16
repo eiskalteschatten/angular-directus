554b8255b22c403013d4a472627c67a0ac1d1652
Alex Seifert <alex@alexseifert.com>
2024-05-16 09:39:26 +0200

    Add the page router

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const navItems = [
+  { label: 'Home', to: '/' },
+  { label: 'Blog', to: '/blog' },
+]
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLink :to="navItem.to" v-for="navItem in navItems">
+    <h1>{{ navItem.label }}</h1>
+  </NuxtLink>
+
+  <NuxtPage />
 </template>
