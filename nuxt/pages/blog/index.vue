2129990fa7d49714c907b0cd4a5313505b686760
Alex Seifert <alex@alexseifert.com>
2024-05-16 11:07:21 +0200

    Use the Container component

--- nuxt/pages/blog/index.vue
+++ nuxt/pages/blog/index.vue
@@ -20,14 +20,16 @@ useHead({
 </script>
 
 <template>
-  <PageTitle>Blog</PageTitle>
+  <Container>
+    <PageTitle>Blog</PageTitle>
 
-  <div v-if="posts?.length === 0">No posts found</div>
-  <div v-else>
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <NuxtLink :to="`/blog/${post.slug}`">{{ post.title }}</NuxtLink>
-      </li>
-    </ul>
-  </div>
+    <div v-if="posts?.length === 0">No posts found</div>
+    <div v-else>
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <NuxtLink :to="`/blog/${post.slug}`">{{ post.title }}</NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </Container>
 </template>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <Container>
     Home!
-  </div>
+  </Container>
 </template>
